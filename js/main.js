83ff103efb0b586e54ec3b0e1764a0b157ad8bd7
sebastian <sebasr1268@gmail.com>
2023-10-18 21:23:02 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let contenedor=document.querySelector(".containers")
+const URL_PELIS='https://api.themoviedb.org/3/trending/movie/day?api_key=b53b30728ee622cb860092e73f1b0f1c&page=4&languaje=es-US'
+
+    fetch(URL_PELIS)
+    .then(resolve => resolve.json())
+    .then(datos=> {
+        console.log(datos)
+        let peliculas=datos.results
+        console.log(peliculas)
+        añadirPelis(peliculas)
+    })
+
+function añadirPelis(peliculas){
+    peliculas.forEach(element => {
+        let div = document.createElement("div")
+        div.classList.add("contents")
+        let images = document.createElement("img")
+        let titulo = document.createElement("h3")
+        titulo.textContent=element.title || element.original_name
+        images.src=`https://image.tmdb.org/t/p/w200/${element.poster_path}`
+        div.appendChild(images)
+        div.appendChild(titulo)
+        contenedor.appendChild(div)
+    });
+
+}
+
---
+++ js/personajes.js
@@ -0,0 +1,26 @@
+let contenedor=document.querySelector(".container")
+const URL_PERSO='https://api.themoviedb.org/3/person/popular?api_key=b53b30728ee622cb860092e73f1b0f1c&page=1&language=en-US'
+
+    fetch(URL_PERSO)
+    .then(resolve => resolve.json())
+    .then(datos=> {
+        console.log(datos)
+        let personajes=datos.results
+        // console.log(peliculas)
+        añadirPersonajes(personajes)
+    })
+    
+    function añadirPersonajes(personajes){
+        personajes.forEach(element => {
+            let div = document.createElement("div")
+            div.classList.add("contents")
+            let images = document.createElement("img")
+            let titulo = document.createElement("h3")
+            titulo.textContent=element.name
+            images.src=`https://image.tmdb.org/t/p/w200/${element.profile_path}`
+            div.appendChild(images)
+            div.appendChild(titulo)
+            contenedor.appendChild(div)
+        });
+    
+    }
